6020658c2f76ab5bf450b8862d4b18d41f9783d7
Maggie-Sarmiento <gsmaggie001@gmail.com>
2022-01-31 20:22:27 -0600

    trayendo datos de ghibli.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,12 @@
+// importar desde ghibli json
+// import jsonFilms from './data/ghibli/ghibli.json' assert { type: 'json' };
+
+
 // estas funciones son de ejemplo
 
-export const example = () => {
+export const films = () => {
   return 'example';
-};
+}; 
 
 export const anotherExample = () => {
   return 'OMG';
