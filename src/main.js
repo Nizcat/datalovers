2b4329cf34893bc6bba7e6b4d64cf354be2a4edf
Maggie-Sarmiento <gsmaggie001@gmail.com>
2022-02-08 21:07:57 -0600

    show info card

--- src/main.js
+++ src/main.js
@@ -4,28 +4,83 @@ import studio from './data/ghibli/ghibli.js';
 import Films from './data.js';
 
 const filmsDom = document.getElementById("container_cards")
+const infoFilms = document.getElementById("container_info")
+const studioObj = studio;
+let filmsObj = studioObj.films;
 
-const studioArray = studio;
-let filmsArray = studioArray.films;
+function selectFilm(id) {
+    let filmselected = {}
+    let peopleFilm = {}
+    const keysFilms = Object.keys(filmsObj);
+    for (let i = 0; i < keysFilms.length; i++) {
+        let positionFilm = keysFilms[i];
+        let filmkey = filmsObj[positionFilm];
+        if (filmkey.id === id) {
+            filmselected = filmkey
+            peopleFilm = filmkey.people
+            console.log(peopleFilm);
+        }
+    }
+    const filmInfo = new Films(
+        {
+            id: filmselected.id, 
+            poster: filmselected.poster, 
+            title: filmselected.title,
+            description: filmselected.description,
+            director: filmselected.director,
+            producer: filmselected.producer,
+            release_date: filmselected.release_date,
+        }
+    )
+    showInfoFilm(filmInfo)
+}
+
+function showInfoFilm(filmInfo){
+    document.getElementById("container_cards").innerHTML = ""
+    const hijo = document.createElement("div")
+    hijo.classList.add("info-card")
+    hijo.innerHTML = ` 
+    <div class= "info_container_card">
+        <div class="info_card_img"> 
+            <img src="${filmInfo.poster}" alt="${filmInfo.title}" />
+        </div>
+        <div class="info_card__data">
+            <h3 class = "info_card_title">${filmInfo.title}</h3>
+            <p>${filmInfo.description}</p>
+            <h4>${filmInfo.producer}</h4>
+            <div class="year">
+                <h5>${filmInfo.release_date}</h5>
+            </div>
+        </div>
+        <div class = "returnButton">
+            <img class="retbut" id="back" src="/img/54623azul.png" alt="Regresa a Inicio" />
+        </div>
+    </div>
+    `
+    infoFilms.appendChild(hijo)
+    document.getElementById('back').addEventListener('click', function(){location.reload()})
+}
 
 
 function showFilms(film) {
+    let id = film.getId()
     const hijo = document.createElement("div")
     hijo.classList.add("card")
     hijo.innerHTML = `
-    <div class="card_img">
-        <img src="${film.getPoster()}" alt="${film.getTitle()}" />
+    <div class="card_img" id="${film.getId()}">
+        <img src="${film.getPoster()}" alt="${film.getTitle()}"/>
     </div>
     <div class="card__data">
         <h3 class = "card_title">${film.getTitle()}</h3>
     </div>
     `
     filmsDom.appendChild(hijo)
+    document.getElementById(id).addEventListener('click', function(){selectFilm(id)})
 }
 
 
-for (const films of filmsArray){
-    const film = new Films(films.poster, films.title)
+for (const films of filmsObj){
+    const film = new Films({id: films.id, poster: films.poster, title: films.title})
     showFilms(film)
 }
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -4,24 +4,50 @@ import Films from '../src/data.js';
 
 
 describe('data', () => {
+  const film = new Films(1, 'title', 'description', 'director', 'producer', 'poster', 'date', 'score')
+
   it('should be a function', () => {
     expect(typeof Films).toBe('function');
   });
 
-  it('sets the title', () => {
-    const film = new Films('a', 'b', 'c')
-    film.setTitle('other')
-    expect(film.getTitle()).toEqual('other');
+  it('sets Id', () => {
+    film.setId(2)
+    expect(film.getId()).toEqual(2);
+  })
+
+  it('sets title', () => {
+    film.setTitle('newtitle')
+    expect(film.getTitle()).toEqual('newtitle');
+  })
+
+  it('sets description', () => {
+    film.setDescription('newDescription')
+    expect(film.getDescription()).toEqual('newDescription');
+  })
+
+  it('sets Director', () => {
+    film.setDirector('newDirector')
+    expect(film.getDirector()).toEqual('newDirector');
+  })
+  
+
+  it('sets Producer', () => {
+    film.setProducer('newProducer')
+    expect(film.getProducer()).toEqual('newProducer');
+  })
+
+  it('sets Poster', () => {
+    film.setPoster('newPoster')
+    expect(film.getPoster()).toEqual('newPoster');
+  })
+
+  it('sets Date', () => {
+    film.setDate('newDate')
+    expect(film.getDate()).toEqual('newDate');
+  })
+
+  it('sets Score', () => {
+    film.setScore('newScore')
+    expect(film.getScore()).toEqual('newScore');
   })
 })
-
-/* expect(new Films()).toBeInstanceOf(Films);
-expect(() => {}).toBeInstanceOf(Function);
-expect(new Films()).toBeInstanceOf(Function); */
-
-/* describe('Films', () => {
-  const films = new Films()
-  it('is a function', () => {
-    expect(films instanceof Films).toBe('true');
-  });
-}); */
